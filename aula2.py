65fa44e1166bbab8837d244f8c0ab1e4cb61bdd5
lucas-gab46 <121887128+lucas-gab46@users.noreply.github.com>
2023-01-06 18:42:32 -0300

    Projects Python 2023 - 230106

---
+++ aula2.py
@@ -0,0 +1,6 @@
+# if,elif,else 
+trabalho_terminado = True
+if trabalho_terminado == True:
+  print('ok lets go')
+else:
+  print('no')
---
+++ entrada de dados.py
@@ -0,0 +1,11 @@
+print('olá qual é seu nome?')
+
+nome = input()
+
+print('que nome kawaii' , nome)
+
+print(nome, 'qual sua altura?')
+
+altura = float(input()) 
+
+print(' Que bacana' , nome, 'com', altura, 'voce é considerado uma pessoa alta')
